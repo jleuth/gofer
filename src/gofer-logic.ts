d9bc64419796816007af0f6914f0025b2492ead9
Jake Leuthardt <jakeleuthardt1@gmail.com>
2025-06-27 09:14:16 -0500

    switch to normal import statement for telegram bot to avoid top level await

--- src/gofer-logic.ts
+++ src/gofer-logic.ts
@@ -7,12 +7,10 @@ import dotenv from 'dotenv';
 import OpenAI from "openai";
 import pixelmatch from "pixelmatch";
 import { PNG } from "pngjs";
-
+import TelegramBot from 'node-telegram-bot-api';
 // @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// @ts-ignore
-const TelegramBot = (await import('node-telegram-bot-api')).default;
 dotenv.config({ path: path.join(__dirname, '../.env.local') });
 const openai = new OpenAI();
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN!, { polling: true });
